f61327fe6df97f40e29d0b9dbec5ae25a059ef1b
TheNMEArengee <jonsay157@gmail.com>
2020-11-15 12:12:17 -0800

    tutorial window draft 1

---
+++ cmpe133chessproject/src/application/tutorialController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+
+public class tutorialController {		
+		
+	public void goToHomePage(ActionEvent event) throws IOException {
+		BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("homepage.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+}
