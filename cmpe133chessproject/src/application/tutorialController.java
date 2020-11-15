801119e106c5eedbe0a591a4aac85df41bdf6613
TheNMEArengee <jonsay157@gmail.com>
2020-11-15 12:28:47 -0800

    added button functionality to home page and tutorial buttons

--- cmpe133chessproject/src/application/tutorialController.java
+++ cmpe133chessproject/src/application/tutorialController.java
@@ -14,7 +14,7 @@ public class tutorialController {
 		
 	public void goToHomePage(ActionEvent event) throws IOException {
 		BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("homepage.fxml"));
-		Scene scene = new Scene(root,400,400);
+		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		window.setScene(scene);
